aac46ea25f06bff816a185543c83caba030378f4
rmitsubayashi <ryomitsu@gmail.com>
2020-02-10 13:41:24 +0900

    can go to settings

--- app/src/main/java/com/github/rmitsubayashi/slackrighttodisconnect/MainActivity.kt
+++ app/src/main/java/com/github/rmitsubayashi/slackrighttodisconnect/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +18,8 @@ class MainActivity: AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.home_menu_settings -> {
-                //navigateToSettings()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
 }
