25ddbefbe63305a889cb11f6e8b642ebc55e1fac
rmitsubayashi <ryomitsu@gmail.com>
2020-02-14 22:07:09 +0900

    design app bar

--- app/src/main/java/com/github/rmitsubayashi/slackrighttodisconnect/MainActivity.kt
+++ app/src/main/java/com/github/rmitsubayashi/slackrighttodisconnect/MainActivity.kt
@@ -5,13 +5,40 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupAppBar()
     }
+
+    private fun setupAppBar() {
+        setSupportActionBar(toolbar)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfig = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfig)
+        hideAppBarTitle()
+    }
+
+    private fun hideAppBarTitle() {
+        // initial screen
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        // after navigating, ↑ is overridden by the navigation controller..
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.settings_fragment -> {toolbar.title = "Settings"}
+                else -> {toolbar.title = ""}
+            }
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_app_bar_home, menu)
         return true
